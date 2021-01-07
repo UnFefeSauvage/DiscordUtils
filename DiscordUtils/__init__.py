32c12ac4fbbab2703de4b05efb514367e98ec894
Felix Bardy <vpcf@bardy.fr>
2021-01-07 13:10:03 +0100

    Version 0.3

--- DiscordUtils/__init__.py
+++ DiscordUtils/__init__.py
@@ -2,7 +2,7 @@ from DiscordUtils import pagination
 from DiscordUtils.invitetracker import InviteTracker
 from DiscordUtils.music import Music
 
-__title__ = "DiscordUtils"
-__version__ = "1.2.6"
-__author__ = "toxicrecker"
+__title__ = "DiscordUtils-UFS"
+__version__ = "0.3"
+__author__ = "UnFefeSauvage"
 __license__ = "MIT"
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8", errors="ignore") as fh:
 
 setuptools.setup(
     name="DiscordUtils_UFS",
-    version="0.2",
+    version="0.3",
     author="UnFefeSauvage",
     description="DiscordUtils is a very useful library made to be used with discord.py",
     long_description=long_description,
